0709e144904bc3fd2b3ea2fa05a538121e9af4c3
A0Dev0Knight <112327676+A0Dev0Knight@users.noreply.github.com>
2023-10-28 16:30:14 +0300

    Player jump
    acum playerul nu mai cade de pe harta dar pluteste in unele zone

--- Jam Game/Assets/Scripts/Player_Movement.cs
+++ Jam Game/Assets/Scripts/Player_Movement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
@@ -10,8 +12,10 @@ public class Player_Movement : MonoBehaviour
 
     //parametrii legati de saritura
     [SerializeField] Transform GroundCheck;
-    [SerializeField] ContactFilter2D Filter;
-    bool _isGrounded;
+    [SerializeField] ContactFilter2D Filter ;
+    [SerializeField] float FloorHeight =0.5f;
+    [SerializeField] LayerMask GroundMask;
+     bool _isGrounded;
     Collider2D[] results = new Collider2D[1];
    
     float velocityY;
@@ -21,7 +25,6 @@ public class Player_Movement : MonoBehaviour
         velocityY += Physics2D.gravity.y * GravityScale * Time.deltaTime;
         
         Vector2 inputVector = Vector2.zero;
-
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             inputVector.x = 1;
@@ -30,9 +33,19 @@ public class Player_Movement : MonoBehaviour
         {
             inputVector.x = -1;
         }
-       
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+
+
+        if (Physics2D.OverlapBox(GroundCheck.position, GroundCheck.localScale, 0, Filter, results)>0 && velocityY < 0)
         {
+            velocityY = 0;
+            Vector2 surface = Physics2D.ClosestPoint(GroundCheck.position, results[0]) + Vector2.up * FloorHeight;
+           // transform.position = new Vector3(transform.position.x, surface.y, 0);
+            _isGrounded = true;
+        }
+        //else _isGrounded = false;
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) ) && _isGrounded)
+        {
+            _isGrounded = false;
             velocityY = Mathf.Sqrt(-2 * JumpHeight * (Physics2D.gravity.y * GravityScale));
         }
 
@@ -42,20 +55,7 @@ public class Player_Movement : MonoBehaviour
         //translatez inputul pe transformul playerului
         Vector3 moveDir = new Vector3(inputVector.x, velocityY,0f);
         transform.position += moveDir * MovementSpeed * Time.deltaTime;
-
-
+        Debug.Log(_isGrounded);
 
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log(collision);
-    }
-
-
-    void Jump()
-    {
-        
-    }
-    
 }
