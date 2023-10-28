9a49775f86ff52301b224ef5fad6c464ac540c7c
A0Dev0Knight <112327676+A0Dev0Knight@users.noreply.github.com>
2023-10-28 11:59:45 +0300

    added ground check
    Momentan nu merge

--- Jam Game/Assets/Scripts/Player_Movement.cs
+++ Jam Game/Assets/Scripts/Player_Movement.cs
@@ -5,8 +5,25 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] float MovementSpeed = 10f;
+
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] float GroundDistance = 0.4f;
+    [SerializeField] LayerMask GroundMask;
+
+    Rigidbody2D _rigidbody2D;
+    BoxCollider2D _playerCollider2D;
+    bool _isGrounded = false;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerCollider2D = GetComponent<BoxCollider2D>();
+    }
+
     private void Update()
     {
+        _isGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundDistance,GroundMask);
+        Debug.Log(_isGrounded);
         Vector2 inputVector = Vector2.zero;
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
@@ -17,14 +34,25 @@ public class Player_Movement : MonoBehaviour
         {
             inputVector.x = -1;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Jump!");
+            Jump();
         }
 
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, inputVector.y,0f);
         transform.position += moveDir * MovementSpeed * Time.deltaTime;
-        Debug.Log(inputVector);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision);
+    }
+
+    void Jump()
+    {
+            
+    }
+    
 }
